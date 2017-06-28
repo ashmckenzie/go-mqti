ccbdbf6517ef67b9a4a583a86f54916bf24121b1
Ash McKenzie <ash@the-rebellion.net>
2017-06-28 18:56:38 +1000

    Tidied up commands/root.go

--- littlefly/commands/root.go
+++ littlefly/commands/root.go
@@ -1,13 +1,11 @@
 package commands
 
 import (
-	"errors"
 	"fmt"
 	"os"
 
 	"github.com/ashmckenzie/go-littlefly/littlefly"
 	"github.com/spf13/cobra"
-	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
 )
 
@@ -20,9 +18,6 @@ var RootCmd = &cobra.Command{
 	Short:         "MQTT subscriber that pumps data into InfluxDB",
 	Long:          `MQTT subscriber that pumps data into InfluxDB`,
 	SilenceErrors: true,
-	// PreRunE: func(cmd *cobra.Command, args []string) error {
-	//   return CheckRequiredFlags(cmd.Flags())
-	// },
 	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
 		if err := validateInput(); err != nil {
 			return err
@@ -37,32 +32,6 @@ var RootCmd = &cobra.Command{
 			fmt.Println(littlefly.Version)
 		}
 	},
-}
-
-// CheckRequiredFlags ...
-func CheckRequiredFlags(flags *pflag.FlagSet) error {
-	requiredError := false
-	flagName := ""
-
-	flags.VisitAll(func(flag *pflag.Flag) {
-		requiredAnnotation := flag.Annotations[cobra.BashCompOneRequiredFlag]
-		if len(requiredAnnotation) == 0 {
-			return
-		}
-
-		flagRequired := requiredAnnotation[0] == "true"
-
-		if flagRequired && !flag.Changed {
-			requiredError = true
-			flagName = flag.Name
-		}
-	})
-
-	if requiredError {
-		return errors.New("Required flag `" + flagName + "` has not been set")
-	}
-
-	return nil
 }
 
 func validateInput() error {
