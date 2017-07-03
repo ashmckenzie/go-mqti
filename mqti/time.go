5bc6fa229923aa8bd1c1112341c1ca34e1cc799b
Ash McKenzie <ash@the-rebellion.net>
2017-07-03 20:29:09 +1000

    Remove global err error var

--- mqti/time.go
+++ mqti/time.go
@@ -5,13 +5,12 @@ import (
 	"time"
 )
 
-var err error
-
 // EndOfTime ...
 const EndOfTime string = "9999-12-31T23:59:59"
 
 // ParseEpoch ...
 func ParseEpoch(in string) time.Time {
+	var err error
 	var i int64
 
 	if i, err = strconv.ParseInt(in, 10, 64); err != nil {
@@ -23,6 +22,7 @@ func ParseEpoch(in string) time.Time {
 
 // ParseTime ...
 func ParseTime(in string) time.Time {
+	var err error
 	var t time.Time
 
 	if t, err = time.Parse("2006-01-02T15:04:05", in); err != nil {
